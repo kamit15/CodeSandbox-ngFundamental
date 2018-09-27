4e5746dd1f52b66613b774c0ee233b112222bd53
Amit Kumar <amit.k15@outlook.com>
2018-09-27 14:00:44 +0530

    Creating First Data-bound Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 
 import { EventsAppComponent } from "./events-app.component";
-
+import { EventsListComponent } from "./events/events-list.component";
 @NgModule({
-  declarations: [EventsAppComponent], //Add Component, Pipe, Directive
+  declarations: [EventsAppComponent, EventsListComponent], //Add Component, Pipe, Directive
   imports: [BrowserModule], //Importing other modules, makes all of its exported declaration and providers available to this module
   providers: [], // Services
   bootstrap: [EventsAppComponent]
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,8 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "events-app",
-  template: `<h1>Hello </h1>
-  <img src="/assets/images/basic-shield.png"/> <!-- path relative to index.html file-->
+  template: `<events-list></events-list>
   `
 })
 export class EventsAppComponent {}
