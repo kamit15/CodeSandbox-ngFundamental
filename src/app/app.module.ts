ee34839370a4ab3f41a959deeb72bbea6ae83551
Amit Kumar <amit.k15@outlook.com>
2018-09-27 13:29:40 +0530

    Accessing Static Files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,9 @@ import { NgModule } from "@angular/core";
 import { EventsAppComponent } from "./events-app.component";
 
 @NgModule({
-  declarations: [EventsAppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  declarations: [EventsAppComponent], //Add Component, Pipe, Directive
+  imports: [BrowserModule], //Importing other modules, makes all of its exported declaration and providers available to this module
+  providers: [], // Services
   bootstrap: [EventsAppComponent]
 })
 export class AppModule {}
--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,6 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: "events-app",
-  template: "<h1>Hello </h1>"
+  template: `<h1>Hello </h1>
+  <img src="/assets/images/basic-shield.png"/> <!-- path relative to index.html file-->
+  `
 })
 export class EventsAppComponent {}
