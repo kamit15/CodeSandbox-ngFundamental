8d37f5bdb73285130808ab9bbbfce3b19ffcfbe1
Amit Kumar <amit.k15@outlook.com>
2018-09-27 15:44:52 +0530

    Styling Components

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,10 +8,25 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 		<div>Date: {{event.date}}</div>
 		<div>Time: {{event.time}}</div>
 		<div>Price: \${{ event.price }}</div>
+    <div>
+      <span>Location: {{event.location.address}}</span>
+      <!--<span>&nbsp;</span>-->
+      <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
+    </div>
     <button class="btn btn-primary" (click)="handleClickMe()">
     Click me!</button>
 	</div>
-  `
+  `,
+  styles: [
+    `
+      .pad-left {
+        margin-left: 10px;
+      }
+      .well div {
+        color: #bbb;
+      }
+    `
+  ]
 })
 export class EventThumbnailComponent {
   @Input() event: any;
