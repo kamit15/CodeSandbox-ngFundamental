f2ff6db2b5358db81e11c4262b075184783d7bc6
Amit Kumar <amit.k15@outlook.com>
2018-09-28 11:32:05 +0530

    Hiding Content with the [hidden] binding
    ngIf = removes element from dom completely.
    hidden applies hidden html attribute to the dom element.
    Better performance if we have to hide and show the element frequently.

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -8,12 +8,14 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
 		<div>Date: {{event?.date}}</div>
 		<div>Time: {{event?.time}}</div>
 		<div>Price: \${{ event?.price }}</div>
-    <div *ngIf="event?.location">
+    <div [hidden]="!event?.location">
+    <!--<div *ngIf="event?.location">-->
       <span>Location: {{event?.location?.address}}</span>
       <!--<span>&nbsp;</span>-->
       <span class="pad-left">{{event?.location?.city}}, {{event?.location?.country}}</span>
     </div>
-    <div *ngIf="event?.onlineUrl">
+    <div [hidden]="!event?.onlineUrl">
+    <!--<div *ngIf="event?.onlineUrl">-->
       Online URL: {{event?.onlineUrl}}
     </div>
     <!--<button class="btn btn-primary" (click)="handleClickMe()">
