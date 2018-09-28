4a198f4d8037d596cf1f1c9231b79034ec3694ac
Amit Kumar <amit.k15@outlook.com>
2018-09-28 10:32:57 +0530

    Repeating data with *ngFor Structural Directive

--- src/app/events/event-thumbnail.component.ts
+++ src/app/events/event-thumbnail.component.ts
@@ -13,12 +13,13 @@ import { Component, Input, Output, EventEmitter } from "@angular/core";
       <!--<span>&nbsp;</span>-->
       <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
     </div>
-    <button class="btn btn-primary" (click)="handleClickMe()">
-    Click me!</button>
+    <!--<button class="btn btn-primary" (click)="handleClickMe()">
+    Click me!</button>-->
 	</div>
   `,
   styles: [
     `
+      .thumbnail { min-height: 210px;}
       .pad-left {
         margin-left: 10px;
       }
